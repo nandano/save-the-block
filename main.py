67f05d66e8ddc8c7a2ae62bebc3c8f899f986bdb
nandano <193489667+nandano@users.noreply.github.com>
2025-04-06 21:39:46 +0530

    Add timer to show elapsed time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -13,9 +14,13 @@ PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
 
-def draw(player):
+FONT = pygame.font.SysFont("comicsans", 30)
+
+def draw(player, elapsed_time):
     WINDOW.blit(background, (0, 0))
 
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WINDOW.blit(time_text, (10, 10))
     pygame.draw.rect(WINDOW, "red", player)
 
     pygame.display.update()
@@ -26,9 +31,12 @@ def main():
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
 
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -41,7 +49,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
             player.x += PLAYER_VEL
         
-        draw(player)
+        draw(player, elapsed_time)
     
     pygame.quit()
 
