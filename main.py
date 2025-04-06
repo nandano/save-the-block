19db5fea07c5c304ec7d8fcb9e77850721baef78
nandano <193489667+nandano@users.noreply.github.com>
2025-04-06 21:09:49 +0530

    Add function to load background image

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ WIDTH, HEIGHT = 1000, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Save The Block")
 
+background = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT))
+
+def draw():
+    WINDOW.blit(background, (0, 0))
+    pygame.display.update()
+
 def main():
     run = True
 
@@ -14,6 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        draw()
     
     pygame.quit()
 
