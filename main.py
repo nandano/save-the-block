c4860f512860c9b62b86f498bb4242591c56a748
nandano <193489667+nandano@users.noreply.github.com>
2025-04-06 21:28:33 +0530

    Add a block that can move in x axis

--- main.py
+++ main.py
@@ -8,20 +8,40 @@ pygame.display.set_caption("Save The Block")
 
 background = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 5
+
+def draw(player):
     WINDOW.blit(background, (0, 0))
+
+    pygame.draw.rect(WINDOW, "red", player)
+
     pygame.display.update()
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
         
-        draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
+            player.x += PLAYER_VEL
+        
+        draw(player)
     
     pygame.quit()
 
